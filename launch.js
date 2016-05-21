470a05bc17a9b0f8c4e8d58cab22b8931b80f2b4
Kevin Hodges <kev_hodges_1@hotmail.com>
2016-05-21 14:45:31 +0100

    redis wrapper and basic drop implementation

--- launch.js
+++ launch.js
@@ -4,26 +4,89 @@
 // get Elephant and frickin use it!
 var cacheWrapper = require( './index' );
 
-cacheWrapper.initialise( '', [ {
+
+cacheWrapper.initialise( {
+  "host": "localhost",
+  "port": "6379",
+  "partition": "cacheWrapper" 
+}, [ {
   segment: 'foo',
   expiresIn: 10000
 } ] ).then( function() {
   cacheWrapper.set( {
     segment: 'foo',
-    key: 'bar',
+    key: 'kev:hodges:1',
     value: 'baz'
   } ).then( function() {
-    cacheWrapper.get( {
+
+    cacheWrapper.set( {
       segment: 'foo',
-      key: 'bar'
-    } ).then( console.log );
-    cacheWrapper.get( {
-      segment: 'foo',
-      key: 'bar'
-    } ).then( console.log );
-    cacheWrapper.get( {
-      segment: 'foo',
-      key: 'bar'
-    } ).then( console.log );
+      key: 'kev:hodges:2',
+      value: 'also baz'
+    } ).then( function() {
+
+      cacheWrapper.set( {
+        segment: 'foo',
+        key: 'kev:nugget:1',
+        value: 'nuggets'
+      } ).then( function() {
+
+        cacheWrapper.delete( {
+          segment: 'foo',
+          prefix: 'kev:hodges:'
+        } ).then( function() {
+
+          cacheWrapper.get( {
+            segment: 'foo',
+            key: 'kev:hodges:1'
+          } ).then( console.log );
+
+          cacheWrapper.get( {
+            segment: 'foo',
+            key: 'kev:hodges:2'
+          } ).then( console.log );
+
+          cacheWrapper.get( {
+            segment: 'foo',
+            key: 'kev:nugget:1'
+          } ).then( console.log );
+
+        } );
+      } );
+
+    } );
+
+
+    
+   
   } ).fail( console.log );
 } ).fail( console.log );
+
+
+// cacheWrapper.initialise( {
+//   "host": "localhost",
+//   "port": "6379",
+//   "partition": "cahceWrapper" 
+// }, [ {
+//   segment: 'foo',
+//   expiresIn: 10000
+// } ] ).then( function() {
+//   cacheWrapper.set( {
+//     segment: 'foo',
+//     key: 'bar',
+//     value: 'baz'
+//   } ).then( function() {
+//     cacheWrapper.get( {
+//       segment: 'foo',
+//       key: 'bar'
+//     } ).then( console.log );
+//     cacheWrapper.get( {
+//       segment: 'foo',
+//       key: 'bar'
+//     } ).then( console.log );
+//     cacheWrapper.get( {
+//       segment: 'foo',
+//       key: 'bar'
+//     } ).then( console.log );
+//   } ).fail( console.log );
+// } ).fail( console.log );
